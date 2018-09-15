e7d851978f2fdb0de781cc6910afde4c261917e5
Nobuoka Yu <nobuoka@vividcode.info>
2018-09-15 13:32:57 +0900

    Docker で動くように

--- db/init.sh
+++ db/init.sh
@@ -4,10 +4,10 @@ ROOT_DIR=$(cd $(dirname $0)/..; pwd)
 DB_DIR="$ROOT_DIR/db"
 BENCH_DIR="$ROOT_DIR/bench"
 
-export MYSQL_PWD=isucon
+export MYSQL_PWD=$DB_PASS
 
-mysql -uisucon -e "DROP DATABASE IF EXISTS torb; CREATE DATABASE torb;"
-mysql -uisucon torb < "$DB_DIR/schema.sql"
+mysql --host "$DB_HOST" -u"$DB_USER" -e "DROP DATABASE IF EXISTS $DB_DATABASE; CREATE DATABASE $DB_DATABASE;"
+mysql --host "$DB_HOST" -u"$DB_USER" "$DB_DATABASE" < "$DB_DIR/schema.sql"
 
 if [ ! -f "$DB_DIR/isucon8q-initial-dataset.sql.gz" ]; then
   echo "Run the following command beforehand." 1>&2
@@ -15,6 +15,6 @@ if [ ! -f "$DB_DIR/isucon8q-initial-dataset.sql.gz" ]; then
   exit 1
 fi
 
-mysql -uisucon torb -e 'ALTER TABLE reservations DROP KEY event_id_and_sheet_id_idx'
-gzip -dc "$DB_DIR/isucon8q-initial-dataset.sql.gz" | mysql -uisucon torb
-mysql -uisucon torb -e 'ALTER TABLE reservations ADD KEY event_id_and_sheet_id_idx (event_id, sheet_id)'
+mysql --host "$DB_HOST" -u"$DB_USER" "$DB_DATABASE" -e 'ALTER TABLE reservations DROP KEY event_id_and_sheet_id_idx'
+gzip -dc "$DB_DIR/isucon8q-initial-dataset.sql.gz" | mysql --host "$DB_HOST" -u"$DB_USER" "$DB_DATABASE"
+mysql --host "$DB_HOST" -u"$DB_USER" "$DB_DATABASE" -e 'ALTER TABLE reservations ADD KEY event_id_and_sheet_id_idx (event_id, sheet_id)'
